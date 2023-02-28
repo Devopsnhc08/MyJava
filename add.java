3a9fe822e97bdb36f805eba1c6a20a08a71de052
Devopsnhc08 <126224203+Devopsnhc08@users.noreply.github.com>
2023-02-28 13:14:23 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,12 +1,17 @@
 //SampleJenkinsProgram
 //hello
 //this is a program
+import java.util.Scanner;
 class add
 {
 	public static void  main(String []args)
 	{  
-	  int a=10,b=10,sum;
-	  sum=a+b;
-	  System.out.println("The sum of 10 and 10 is:"+sum);
+	 Scanner s=new Scanner(system.in);
+	System.out.printline("enter value for A and B:");
+	int A=s.nextInt();
+	int B=s.nextInt();
+	int sum;
+	sum=A+B;
+	System.out.printline("the sum of {0} and {1} is:"+sum );
 	}
 }
